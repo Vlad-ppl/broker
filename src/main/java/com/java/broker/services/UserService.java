69f67623efb92f4dad75bdd076609e3785a3e20e
Vlad-ppl <melnik.vlad1990@gmail.com>
2025-04-21 12:37:16 +0200

    add modific-UserService

--- src/main/java/com/java/broker/services/UserService.java
+++ src/main/java/com/java/broker/services/UserService.java
@@ -3,25 +3,21 @@ package com.java.broker.services;
 import com.java.broker.dto.UserDto;
 import com.java.broker.entity.UserEntity;
 import com.java.broker.repository.UserRepository;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final DataSourceTransactionManager dataSourceTransactionManager;
-
-    public UserService(UserRepository userRepository, DataSourceTransactionManager dataSourceTransactionManager) {
-        this.userRepository = userRepository;
-        this.dataSourceTransactionManager = dataSourceTransactionManager;
-    }
 
     public List<UserEntity> findAll() {
         return userRepository.findAll();
     }
+
     public UserEntity save(UserDto userDto) {
         if (userRepository.findByEmail(userDto.getEmail()).isPresent()) {
             throw new IllegalStateException("User with email " + userDto.getEmail() + " already exists");
