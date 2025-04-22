9ff0d2c90ba8a7d97b1abeb82dcbd5a590b78de3
Vlad-ppl <melnik.vlad1990@gmail.com>
2025-04-22 18:58:32 +0200

    + rename BinanceBalanceController

--- src/main/java/com/java/broker/controller/BinanceBalanceController.java
+++ src/main/java/com/java/broker/controller/BinanceBalanceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/binance")
 @RequiredArgsConstructor
-public class BinanceAccountController {
+public class BinanceBalanceController {
 
     private final BinanceBalanceService binanceAccountService;
 
